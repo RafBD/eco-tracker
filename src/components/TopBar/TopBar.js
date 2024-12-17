d125177f7100a0025a0ea829d1e5487fa0c81f68
RafaelBD <rafa.bediaz@gmail.com>
2024-12-17 14:21:36 -0600

    Add TopBar component with responsive styles and integrated SideBar

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SideBar from '../SideBar/SideBar';
+import './TopBar.css';
+
+const TopBar = () => {
+
+    return (
+        <div className="topbar">
+            <SideBar />
+        </div>
+    )
+}
+
+export default TopBar;
