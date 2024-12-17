69e981646728a83bcab3fa5ef6b3d1340f4a4472
RafaelBD <rafa.bediaz@gmail.com>
2024-12-17 14:17:57 -0600

    Refactor App component to implement routing and update layout structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Topbar from "./components/TopBar/TopBar";
+import Home from "./components/Home/Home";
+import AuditPage from "./components/Results/AuditResults";
+import Dashboard from "./components/Dashboard/DashBoard";
+import FileUpload from "./components/FileUploader/FileUploader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Topbar />
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/audit" element={<AuditPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/fileUpload" element={<FileUpload />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
